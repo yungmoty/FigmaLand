59322988e349bf242a9db5d574d51de60b4a44eb
yungmoty <ycatbiy64@gmail.com>
2023-05-17 17:44:54 +0300

    Create header
    Add adaptive

---
+++ js/common.js
@@ -0,0 +1,10 @@
+// Burger
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
