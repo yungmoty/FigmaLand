56ec1f156bd40d5d794396c5409c732fa7282e20
yungmoty <ycatbiy64@gmail.com>
2023-05-18 12:11:30 +0300

    Create footer
    Add an adaptive and a menu accrodion

--- js/common.js
+++ js/common.js
@@ -1,4 +1,32 @@
-// Burger
+
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	Blackberry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.Blackberry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows()
+		);
+	}
+}
+
+//\\burger\\//
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 if (iconMenu) {
@@ -8,3 +36,29 @@ if (iconMenu) {
 		menuBody.classList.toggle('_active');
 	});
 }
+//\\burger//\\
+
+
+//\\accordion\\//
+if (isMobile.any()) {
+	const accordions = document.querySelectorAll('._accordion');
+
+	accordions.forEach(element => {
+		element.addEventListener('click', (e) => {
+			const target = e.currentTarget;
+			let menuFooter = target.querySelector('.footer__title');
+			let menuFooterList = target.querySelector('.footer__list');
+
+			if (target.querySelector('.footer__title')) {
+				menuFooter.parentElement.classList.toggle('_active');
+
+				if (menuFooter.parentElement.classList.contains('_active')) {
+					menuFooterList.style.maxHeight = menuFooterList.scrollHeight + 'px';
+				} else {
+					menuFooterList.style.maxHeight = null;
+				}
+			}
+		});
+	})
+}
+//\\accordion//\\
